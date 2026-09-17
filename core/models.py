3be0f0e02ce219065d962633ce97d41baaa3ec05
Vanderlei Castro <vanderlei.castro055@gmail.com>
2026-09-17 17:24:00 -0300

    Projeto Finalizado

---
+++ core/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models.fields import CharField
+
+
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço',decimal_places=2,max_digits=8)
+    estoque = models.IntegerField('Quantidade em Estoque')
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome',max_length=100)
+    email = models.EmailField('Email',max_length=100)
+
+    def __str__(self) -> str:
+        return f'{self.nome} {self.sobrenome}'
---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.urls import urlpatterns
+from django.urls import path
+from .views import index,contato,produto
+urlpatterns = [
+    path('',index, name='index'),
+    path('contato',contato, name='contato'),
+    path('produto/<int:pk>', produto, name='produto')
+]
