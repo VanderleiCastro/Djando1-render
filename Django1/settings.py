74823a6e5eae829993ae040a02fa7ae07d243a84
Vanderlei Castro <vanderlei.castro055@gmail.com>
2026-09-20 15:29:45 -0300

    tentando subir o codigo com o tratamento de pagina

--- Django1/settings.py
+++ Django1/settings.py
@@ -28,10 +28,10 @@ SECRET_KEY = 'django-insecure-)q%p)!_18=u^j_8i1h=)ag@%ud*8q)74zd9^$!p#@=m)b+qd7=
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG com valor True é modo desenvolvimento
 # DEBUG com valor False é modo de produção
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = ['djando1-render.onrender.com']
+#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['djando1-render.onrender.com']
 
 
 # Application definition
