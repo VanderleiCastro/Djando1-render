d94a711536837838e32690fa1daef37257f24174
Vanderlei Castro <vanderlei.castro055@gmail.com>
2026-09-20 14:03:07 -0300

    Nova configuração para o Admin, tratando erro

--- core/views.py
+++ core/views.py
@@ -29,11 +29,11 @@ def produto(request,pk):
 
 def pagina_nao_encontrada(request,exception):
     """trata o erro 404"""
-    return render(request,'errors/404.html',status=404)
+    return render(request,'templates/404.html',status=404)
 
 def erro_interno_servidor(request):
     """trata o erro 500"""
-    return render(request,'errors/500.html',status=500)
+    return render(request,'templates/500.html',status=500)
 
 '''def error404(request,exception):
     template = loader.get_template('404.html')
